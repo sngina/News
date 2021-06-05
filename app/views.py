05ffd2111ee1053470028b89dca02a6e54db7bbb
sngina <sngina707@gmail.com>
2021-06-05 12:30:13 +0300

    add a function that returns index page and its data

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from flask import render_template
 from app import app
 
+@app.route('/')
+def index():
+  # a function that return the index page and its data.....
+
+    message = 'Hello News'
+    return render_template('index.html')
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    # function that returns the news details page and its data
+
+    return render_template('news.html', id = news_id)
+@app.route('/news/<int:news_id>')
+def  news(news_id):
+    #function that returns the movie details page and its data
+    return render_template('news.html', id = news_id)
