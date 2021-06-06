5c17f01252331936f888a5e717abc028b34c9f5c
sngina <sngina707@gmail.com>
2021-06-06 10:16:33 +0300

    add an index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from  flask import  Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 app = Flask (__name__,instance_relative_config = True) 
 
 #setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#initialzing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -8,10 +8,11 @@ def index():
   # function that returns the index page and its data
 
   #getting trending news
-  popular_news = get_news('popular')
-  print(popular_news)
+  trending_news = get_news('popular')
+  now_showing_news = get_news('now_showing_news')
+  print(trending_news)
   title = 'Home -Welcome to the News Website Online'
-  return render_template('index.html')
+  return render_template('index.html', title = title, trending = trending_news, now_showing = now_showing_news)
 
 
 @app.route('/news/<int:news_id>')
